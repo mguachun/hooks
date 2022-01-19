8c9bd3b58a1518f906366750bc795936a0630f99
Melissa Guachun <1212malka@gmail.com>
2022-01-19 17:26:14 -0500

    simple hooks practice with counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(4)
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={decrementCount}>-</button>
+    <span>{count}</span>
+    <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
 export default App;
+//we use destructuring to import useState 
+//useState always returns an array with two values
+//useState must be called in top level of function
+//useState only work in order they are written
+//only work in functionial components
